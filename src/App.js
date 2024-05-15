fe08f6a7a635e5cdb4fc7e469d81ca8e864acfa5
soncreux <dimakarnur@gmail.com>
2024-05-16 02:19:47 +0300

    Routing started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from './pages/menu'; // Import the Menu component
 import Main from "./pages/main";
-import Navbar from "./pages/components/Navbar"
+//import Navbar from "./pages/components/Navbar"
 function App() {
   return (
-    <Menu/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="menu" element={<Menu />} />
+        
+      </Routes>
+    </BrowserRouter>
   );
 }
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 export default App;
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import "./components/styles/main.css"
-function Main(){
+function Main (){
     return (
     <><Navbar /><div class="buttons-container">
             <button class="button">Orders</button>
